e5c93a047d519b5e911bfc2a768a33fa315aca33
Iskilu Rasheed Akanni <66404246+Raashot12@users.noreply.github.com>
2021-01-09 15:12:41 +0100

    Vue Internship task(ft:completed)

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ const app = new Vue({
 	
 	}
 }); 
+
+const ppl = [ {
+	name : 'Jim',
+	status: ['Married'],
+},
+ {
+	 name: 'Anna',
+	 status: ['Married', 'Parent'],
+ },
+ {
+	 name: 'Ed',
+	 status: ['Married']
+ }
+];
+
+const print = ppl.find(person => person.status.includes('parent'))
+console.log(print());
+
+/**
+ * ?The includes() method determines whether a string contains the characters of a specified string. 
+ * ?This method returns true if the string contains the characters, and false if not. Note: The includes() method is case sensitive.
+ */
